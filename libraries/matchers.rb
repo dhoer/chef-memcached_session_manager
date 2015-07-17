1175ef98f886801ca7381147f73be62b873cb7ee
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-16 17:01:38 -0700

    Refactor context

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,5 +1,5 @@
 if defined?(ChefSpec)
-  def create_memcached_session_manager_context(name)
-    ChefSpec::Matchers::ResourceMatcher.new(:memcached_session_manager, :create_memcached_session_manager_context, name)
+  def install_memcached_session_manager_context(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:memcached_session_manager_context, :install, name)
   end
 end
--- spec/unit/context_spec.rb
+++ spec/unit/context_spec.rb
@@ -30,8 +30,36 @@ describe 'memcached_session_manager_test::context_update' do
 
   it 'creates context manager template' do
     expect(chef_run).to create_template('/var/chef/cache/context_manager.xml').with(
-      source: 'context_manager.xml.erb',
-      cookbook: 'memcached_session_manager'
-    )
+        source: 'context_manager.xml.erb',
+        cookbook: 'memcached_session_manager'
+      )
+  end
+end
+
+describe 'memcached_session_manager_test::context_remove' do
+  let(:chef_run) do
+    ChefSpec::SoloRunner.new(
+      file_cache_path: '/var/chef/cache',
+      platform: 'centos',
+      version: '7.0',
+      step_into: 'memcached_session_manager_context'
+    ).converge(described_recipe)
+  end
+
+  it 'copies context manager exists' do
+    expect(chef_run).to create_cookbook_file('/var/chef/cache/context_manager_remove.xml').with(
+        source: 'context_manager_exists.xml'
+      )
+  end
+
+  it 'removes manager element from context xml' do
+    expect(chef_run).to installs_memcached_session_manager_context('/var/chef/cache/context_manager_remove.xml')
+  end
+
+  it 'creates context manager template' do
+    expect(chef_run).to create_template('/var/chef/cache/context_manager.xml').with(
+        source: 'context_manager.xml.erb',
+        cookbook: 'memcached_session_manager'
+      )
   end
 end
