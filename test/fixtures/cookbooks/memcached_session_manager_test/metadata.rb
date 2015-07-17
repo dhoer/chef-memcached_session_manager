d1959814c60e49f533e360cd6a49ec84a76fa898
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-17 16:27:29 -0700

    Fix invalid java constraint

--- test/fixtures/cookbooks/memcached_session_manager_test/metadata.rb
+++ test/fixtures/cookbooks/memcached_session_manager_test/metadata.rb
@@ -6,6 +6,6 @@ description 'Tests memcached_session_manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-depends 'java', '~> 1.32'
+depends 'java', '~> 1.31'
 depends 'tomcat', '~> 0.17'
 depends 'memcached_session_manager'
