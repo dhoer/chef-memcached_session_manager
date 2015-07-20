94265e7693e14b1f0313945531158712b129dd97
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-20 10:48:38 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description "Tomcat high-availability clusters with memcached using Martin Grotzke's memcached-session-manager"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '1.0.0'
 
 supports 'centos'
 supports 'redhat'
